24351eb0483d448dffebd3ecb9f91d6ec0c4702c
Kushagra Mittal <kushagra.mittal@ucdconnect.ie>
2023-05-31 23:01:36 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,57 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
+import java.text.DecimalFormat;
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+public class Main
+{
+    static double Input, Kelvin, Fahrenheit, Celsius;
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+    public static double get_input()
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter Temperature to convert: ");
+        double Input = scanner.nextDouble();
+
+        return Input;
+    }
+
+    public static char Unit(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter Unit (K,C or F): ");
+        char Unit = scanner.next().charAt(0);
+        scanner.close();
+        return Unit;
+    }
+    public static void main(String[] args)
+    {
+        Input = get_input();
+        char Unit = Unit();
+        switch (Unit) {
+            case 'K' -> {
+                Kelvin = Input;
+                Celsius = Kelvin - 273.15;
+                Fahrenheit = ((Celsius * 9) / 5) + 32;
+            }
+            case 'C' -> {
+                Celsius = Input;
+                Kelvin = Celsius + 273.15;
+                Fahrenheit = ((Celsius * 9) / 5) + 32;
+            }
+            case 'F' -> {
+                Fahrenheit = Input;
+                Celsius = (Fahrenheit - 32) * 5 / 9;
+                Kelvin = Celsius + 273.15;
+            }
+            default -> {
+                System.out.println("Please enter valid Unit!");
+                return;
+            }
         }
+
+        DecimalFormat df = new DecimalFormat("#.##");
+        System.out.println(Input + " " + Unit + " is equivalent to:");
+        System.out.println(df.format(Kelvin) + " K or");
+        System.out.println(df.format(Celsius) + " C or");
+        System.out.println(df.format(Fahrenheit) + " F");
+
     }
 }
